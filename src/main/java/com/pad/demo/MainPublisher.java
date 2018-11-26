35c5e576aa9cb7a2bbe4f817a7433ae5bb2bc7e1
Hiruine <radu.madiudin@yahoo.com>
2018-11-26 22:29:50 +0200

    High level demo implementation

--- src/main/java/com/pad/demo/MainPublisher.java
+++ src/main/java/com/pad/demo/MainPublisher.java
@@ -1,9 +1,23 @@
 package com.pad.demo;
 
+import com.pad.Client.Client;
+import com.pad.dto.Message;
+import com.pad.dto.MessageType;
+import com.pad.dto.converter.GsonConverter;
+import com.pad.dto.entity.Student;
+
 public class MainPublisher {
 
     public static void main(String[] args) {
 
-//        Client connection =
+        Client client = new Client("localhost", 1234);
+        client.openConnection();
+        client.createChannel("PAD");
+
+        Message message = new Message();
+        message.setData(GsonConverter.convertToJson(new Student("Radu", "TI-153", 22)));
+        message.setType(MessageType.SEND_MESSAGE);
+        message.setChannelName("PAD");
+
     }
 }
--- src/main/java/com/pad/demo/MainReceiver.java
+++ src/main/java/com/pad/demo/MainReceiver.java
@@ -1,4 +1,18 @@
 package com.pad.demo;
 
+import com.pad.Client.Client;
+import com.pad.dto.converter.GsonConverter;
+import com.pad.dto.entity.Student;
+
 public class MainReceiver {
+
+    public static void main(String[] args) {
+
+        Client client = new Client("localhost", 1234);
+        client.openConnection();
+        client.subscribe("PAD", message -> {
+            Student student = GsonConverter.convertToDto(message.getData(), Student.class);
+            System.out.println(student);
+        });
+    }
 }
